bbad91627588ce059eb4f3a25afde7a2d64c12b0
Sonaliraj <sonali@rubyeffect.com>
2016-02-12 16:38:11 +0530

    finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-def hi
-    render text: "hello to all of u"
-  end
+
 end
